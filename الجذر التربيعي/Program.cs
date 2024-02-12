24759b75fdd040640f6af2ec77da732efa3520db
kc <kc@DESKTOP-METLNKO>
2024-02-12 15:22:52 -0800

    Add project files.

---
+++ الجذر التربيعي/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+void massage(string massageNow)
+{
+    Console.WriteLine(massageNow);
+}
+
+massage($"Welcome To program \n" +
+    "*************************************************** \n"
+  + "please enter number: ");
+
+int number;
+bool isConvert = int.TryParse(Console.ReadLine(), out number);
+if (!isConvert)
+{
+    massage(" please enter valid number");
+    return;
+}
+int some = 0;
+for (int i = 1; i < number; i++)
+
+{
+    
+    some = i * i;
+    if (some == number)
+    {
+        some = i;
+        break;
+    }
+
+}
+
+massage("\n Sqrt is " + some);
+
